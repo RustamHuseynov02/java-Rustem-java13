8574ff1d6b9b13c518478af149a58b6c24dbd54f
RustamHuseynov02 <147059960+RustamHuseynov02@users.noreply.github.com>
2023-10-09 20:53:51 +0400

    ders2
    oyrendiklerimizi tetbiq edirik

---
+++ codes/javase/src/javase/ders2.java
@@ -0,0 +1,13 @@
+package javase;
+
+public class ders2 {
+     public static void main(String[] args) {
+    	 String name = "Rustem";
+    	 String surname = "Huseynov";
+    	 String fullname = name + " " + surname;
+    	 
+    	 System.out.println("Menim tam adim :" + fullname);
+     }
+     
+}
+
