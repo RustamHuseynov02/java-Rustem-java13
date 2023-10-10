fa7db83e3cc8385bf3d96a8999637d8377c97e52
RustamHuseynov02 <147059960+RustamHuseynov02@users.noreply.github.com>
2023-10-10 22:11:21 +0400

    jShellPractice
    ...

---
+++ codes/javase/src/EvIsi/jShellPractice.java
@@ -0,0 +1,46 @@
+package EvIsi;
+
+public class jShellPractice {
+        public static void main(String[] args) {
+        	int age = 7;
+        	age = 14;
+        	System.out.println(age);
+        	
+        	//
+        	
+        	byte b = 7;
+        	System.out.println(b);
+        	
+        	//
+        	
+        	short s = 7;
+        	System.out.println(s);
+        	
+        	//
+        	
+        	long l = 7;
+        	System.out.println(l);
+        	
+        	//
+        	
+        	double d = 7.9D;
+        	System.out.println(d);
+        	
+        	//
+        	
+        	float f = 7.9F;
+        	System.out.println(f);
+        	
+        	//
+        	
+        	char c = 'A';
+        	System.out.println(c);
+        	
+        	//
+        	
+        	boolean hasHome = true;
+        	System.out.println(hasHome);
+        	
+        	
+		}
+}
