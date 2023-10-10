de185f0c2e726cd1e39565d497de7de66f71825c
RustamHuseynov02 <147059960+RustamHuseynov02@users.noreply.github.com>
2023-10-10 21:27:34 +0400

    Calisma1
    Verilen tapsiriqi yerine yetirdim

--- codes/javase/src/javase/ders2.java
+++ codes/javase/src/javase/ders2.java
@@ -4,9 +4,14 @@ public class ders2 {
      public static void main(String[] args) {
     	 String name = "Rustem";
     	 String surname = "Huseynov";
+    	 int age  = 20;
+    	 String city ="Sumqayit";
     	 String fullname = name + " " + surname;
     	 
+    	 
     	 System.out.println("Menim tam adim :" + fullname);
+    	 System.out.println("Menim yasim :" + age);
+    	 System.out.println("Menim seherim :" + city);
      }
      
 }
