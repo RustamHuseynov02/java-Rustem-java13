c2b9e4a54228e1ac0051738067ea49bb844c7877
RustamHuseynov02 <147059960+RustamHuseynov02@users.noreply.github.com>
2023-10-16 21:02:44 +0400

    Tapsiriq2

---
+++ codes/javase/src/javase/Tapsiriq2.java
@@ -0,0 +1,14 @@
+package javase;
+
+public class Tapsiriq2 {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 30; i++) {
+			if(i%7 == 0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
