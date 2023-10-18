8af572fdb071c0c4e620453653e80837c6fa3352
RustamHuseynov02 <147059960+RustamHuseynov02@users.noreply.github.com>
2023-10-18 23:33:48 +0400

    HomeWork2

---
+++ codes/javase/src/EvIsi/EvIsi.java
@@ -0,0 +1,23 @@
+package EvIsi;
+
+public class EvIsi {
+
+	public static void main(String[] args) {
+		
+		int a = 14;
+		
+		while(a <= 39) {
+			System.out.println(++a);
+			
+		}
+		
+		System.out.println("--------");
+		
+		for(int i = 21; i < 50; i +=2) {
+			
+			System.out.println(i);
+		}
+		
+	}
+
+}
