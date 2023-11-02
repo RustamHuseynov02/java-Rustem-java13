2e68bcf9df7566b7c9f69223d8b1993b1a264413
RustamHuseynov02 <147059960+RustamHuseynov02@users.noreply.github.com>
2023-11-02 07:33:29 +0400

    TodayPractice
    .....?

---
+++ codes/javase/src/Practice/Calculator.java
@@ -0,0 +1,11 @@
+package Practice;
+
+public class Calculator {
+      int prop1;
+      int prop2;
+      
+      void calculate(int prop1,int prop2) {
+    	  int result = prop1 + prop2;
+    	  System.out.println(result);
+      }
+}
---
+++ codes/javase/src/Practice/Class.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class Class {
+      public static void main(String[] args) {
+		Calculator c = new Calculator();
+		c.calculate(5, 6);
+		
+		
+	}
+}
