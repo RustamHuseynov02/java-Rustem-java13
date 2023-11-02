139a01751a05ae4485c00e7a97bd745e8430574f
RustamHuseynov02 <147059960+RustamHuseynov02@users.noreply.github.com>
2023-11-02 08:00:54 +0400

    calculateFactorial

---
+++ codes/javase/src/PracticeHome/Calculator.java
@@ -0,0 +1,20 @@
+package PracticeHome;
+
+public class Calculator {
+   
+	int numbers;
+	
+	
+	void calculateFactorial (int number) {
+
+		int result = number * 2 * 3 * 4 * 5;
+		System.out.println(result);
+		
+		
+		//		for (int i = 1; i < 8; i++) {
+//			if (1*i==1) {
+//				System.out.println(i);
+//			}
+//		}
+	}
+}
---
+++ codes/javase/src/PracticeHome/MainClass01.java
@@ -0,0 +1,9 @@
+package PracticeHome;
+
+public class MainClass01 {
+     public static void main(String[] args) {
+		 
+            Calculator calculate = new Calculator();
+            calculate.calculateFactorial(1);
+	}
+}
