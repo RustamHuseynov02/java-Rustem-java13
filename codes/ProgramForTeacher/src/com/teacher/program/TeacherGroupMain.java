716f6e637dc847c7e42473a6c482bfafe466b77c
RustamHuseynov02 <147059960+RustamHuseynov02@users.noreply.github.com>
2023-12-12 15:58:11 +0400

    Update

--- codes/ProgramForTeacher/src/com/teacher/program/TeacherGroupMain.java
+++ codes/ProgramForTeacher/src/com/teacher/program/TeacherGroupMain.java
@@ -1,6 +1,8 @@
 package com.teacher.program;
 
 
+import java.time.LocalDateTime;
+
 import com.teacher.program.entity.TeacherEntity;
 import com.teacher.program.entity.TeacherGroupEntity;
 import com.teacher.program.exception.OurRuntimeException;
@@ -12,11 +14,12 @@ public class TeacherGroupMain {
 	public static void main(String[] args) {
 		
 
-		//TeacherGroupEntity teacherGroup = new TeacherGroupEntity(null, "A1", null, 15);
+		//TeacherGroupEntity teacherGroup = new TeacherGroupEntity(null, "A1", 1, LocalDateTime.of(2022, 12, 15, 20,20), 15);
 		
 		
 		TeacherGroupRepository teacherGroupRepo = new TeacherGroupRepository();
 		try {
+			//teacherGroupRepo.createGroup(teacherGroup);
 			teacherGroupRepo.findById(16);
 			teacherGroupRepo.deleteById(1);
 		} catch (OurRuntimeException e) {
