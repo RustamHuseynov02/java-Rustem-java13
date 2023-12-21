45f5d837c578ffccb4c09bc6449f4896ac8b9f8a
RustamHuseynov02 <147059960+RustamHuseynov02@users.noreply.github.com>
2023-12-21 20:05:24 +0400

    SpringProjectCreate

---
+++ codes/spring/spring-java13/src/main/java/az/developia/springjava13/SpringJava13Application.java
@@ -0,0 +1,13 @@
+package az.developia.springjava13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJava13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJava13Application.class, args);
+	}
+
+}
