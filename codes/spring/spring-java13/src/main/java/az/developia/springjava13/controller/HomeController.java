67c751e0c57edb9c2659de954021ee4d7f9e01d9
RustamHuseynov02 <147059960+RustamHuseynov02@users.noreply.github.com>
2024-01-04 21:01:49 +0400

    update

--- codes/spring/spring-java13/src/main/java/az/developia/springjava13/controller/HomeController.java
+++ codes/spring/spring-java13/src/main/java/az/developia/springjava13/controller/HomeController.java
@@ -9,41 +9,39 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HomeController {
 
-	
-     @GetMapping
-     public String showHome() {
-    	 return "Home";
-     }
-     
-     @GetMapping(path = "/student")
-     public String showStudent(@RequestParam(name = "q") String q) {
-    	 ArrayList<String> students = new ArrayList<>();
-    	 students.add("Kamil");
-    	 students.add("Ilqar");
-    	 students.add("Mehemmed");
-    	 students.add("Subhan");
-    	 
-    	 ArrayList<String> studentsFilter = new ArrayList<>();
-    	 students.stream().forEach(s->{
-    		if (s.contains(q)) {
+	@GetMapping
+	public String showHome() {
+		return "Home";
+	}
+
+	@GetMapping(path = "/student")
+	public String showStudent(@RequestParam(name = "q", required = false, defaultValue = "") String q) {
+		ArrayList<String> students = new ArrayList<>();
+		students.add("Kamil");
+		students.add("Ilqar");
+		students.add("Mehemmed");
+		students.add("Subhan");
+
+		ArrayList<String> studentsFilter = new ArrayList<>();
+		students.stream().forEach(s -> {
+			if (s.contains(q)) {
 				studentsFilter.add(s);
-			} 
-    	 });
-    	 
-    	 System.out.println(studentsFilter);
-    	 
-    	 
-    	 return "students";
-     }
-     
-     @GetMapping(path = "/book")
-     public String showBook() {
-    	 return "book";
-     }
-     
-     @GetMapping(path = "/computers")
-     public String showComputer() {
-    	 return "computer";
-     }
-     
+			}
+		});
+
+		System.out.println(studentsFilter);
+
+		return "students";
+	}
+
+	@GetMapping(path = "/book")
+	public String showBook() {
+		return "book";
+	}
+
+	@GetMapping(path = "/computers")
+	public String showComputer() {
+		return "computer";
+	}
+
 }
