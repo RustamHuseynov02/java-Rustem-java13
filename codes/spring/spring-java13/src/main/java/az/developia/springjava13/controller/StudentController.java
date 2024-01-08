0bcc6a8b502fada3378a5ffa88f499c0666abe73
RustamHuseynov02 <147059960+RustamHuseynov02@users.noreply.github.com>
2024-01-08 17:37:03 +0400

    Update StudentController.java

--- codes/spring/spring-java13/src/main/java/az/developia/springjava13/controller/StudentController.java
+++ codes/spring/spring-java13/src/main/java/az/developia/springjava13/controller/StudentController.java
@@ -4,32 +4,47 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(path = "/students")
 public class StudentController {
 	
 	
 	@GetMapping
-	public String showStudent(@RequestParam(name = "q", required = false, defaultValue = "") String q) {
+	public ArrayList<String> student() {
 		ArrayList<String> students = new ArrayList<>();
 		students.add("Kamil");
 		students.add("Ilqar");
 		students.add("Mehemmed");
 		students.add("Subhan");
-
-		ArrayList<String> studentsFilter = new ArrayList<>();
-		students.stream().forEach(s -> {
-			if (s.contains(q)) {
-				studentsFilter.add(s);
-			}
-		});
-
-		System.out.println(studentsFilter);
-
-		return "students";
+		
+		return students;
+				
 	}
+	
+	
+//	@GetMapping
+//	public String showStudent(@RequestParam(name = "q", required = false, defaultValue = "") String q) {
+//		ArrayList<String> students = new ArrayList<>();
+//		students.add("Kamil");
+//		students.add("Ilqar");
+//		students.add("Mehemmed");
+//		students.add("Subhan");
+//
+//		ArrayList<String> studentsFilter = new ArrayList<>();
+//		students.stream().forEach(s -> {
+//			if (s.contains(q)) {
+//				studentsFilter.add(s);
+//			}
+//		});
+//
+//		System.out.println(studentsFilter);
+//
+//		return "students";
+//	}
 
 }
