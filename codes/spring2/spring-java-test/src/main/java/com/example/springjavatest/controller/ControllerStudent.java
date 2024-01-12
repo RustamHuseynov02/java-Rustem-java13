8aa196b00d232323dc4d0ec22363e654e2871a11
RustamHuseynov02 <147059960+RustamHuseynov02@users.noreply.github.com>
2024-01-12 20:52:39 +0400

    NewTask

--- codes/spring2/spring-java-test/src/main/java/com/example/springjavatest/controller/ControllerStudent.java
+++ codes/spring2/spring-java-test/src/main/java/com/example/springjavatest/controller/ControllerStudent.java
@@ -1,13 +1,37 @@
 package com.example.springjavatest.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.springjavatest.component.Person;
 
 @Controller
 public class ControllerStudent {
 
 	@GetMapping(path = "/students")
-	public String student() {
-		return "students";
+	public ArrayList<String> student(@RequestParam(name = "ad",required = false,defaultValue = "name not") String ad) {
+		ArrayList<String> students = new ArrayList<>();
+		students.add("Kamil");
+		students.add("Ilqar");
+		students.add("Ramil");
+		
+		
+	
+		ArrayList<String> studentsFilter = new ArrayList<String>();
+		students.stream().forEach(s ->{
+			if (s.contains(ad)) {
+				studentsFilter.add(s);
+			}
+			
+			
+		});
+		System.out.println(studentsFilter);
+		return students;
+		
+		
 	}
 }
