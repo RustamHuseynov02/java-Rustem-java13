3786c70e083d46bbc5dd0a149f892a69407c68d2
RustamHuseynov02 <147059960+RustamHuseynov02@users.noreply.github.com>
2024-01-14 14:48:54 +0400

    thymeleaf

--- codes/spring2/spring-java-test/src/main/java/com/example/springjavatest/controller/ControllerStudent.java
+++ codes/spring2/spring-java-test/src/main/java/com/example/springjavatest/controller/ControllerStudent.java
@@ -8,30 +8,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.springjavatest.component.Person;
+import com.example.springjavatest.component.Student;
 
 @Controller
 public class ControllerStudent {
 
 	@GetMapping(path = "/students")
-	public ArrayList<String> student(@RequestParam(name = "ad",required = false,defaultValue = "name not") String ad) {
-		ArrayList<String> students = new ArrayList<>();
-		students.add("Kamil");
-		students.add("Ilqar");
-		students.add("Ramil");
-		
-		
-	
-		ArrayList<String> studentsFilter = new ArrayList<String>();
-		students.stream().forEach(s ->{
-			if (s.contains(ad)) {
-				studentsFilter.add(s);
-			}
-			
-			
-		});
-		System.out.println(studentsFilter);
-		return students;
-		
-		
+	public Student students(Model model) {
+		Student student = new Student(1, "Rustem", "Huseynov");
+		model.addAttribute("id", student.getId());
+		model.addAttribute("name", student.getName());
+		model.addAttribute("surname", student.getSurname());
+		return student;
 	}
+//	public ArrayList<String> student(@RequestParam(name = "ad",required = false,defaultValue = "name not") String ad,Model model) {
+//		ArrayList<String> student = new ArrayList<>();
+//		student.add("Kamil");
+//		student.add("Ilqar");
+//		student.add("Ramil");
+//		
+//		
+//		
+//	
+//		ArrayList<String> studentsFilter = new ArrayList<String>();
+//		student.stream().forEach(s ->{
+//			if (s.contains(ad)) {
+//				studentsFilter.add(s);
+//			}
+//			
+//			
+//		});
+//		model.addAttribute("ad", studentsFilter);
+//		System.out.println(studentsFilter);
+//		return student;
+		
+		
 }
