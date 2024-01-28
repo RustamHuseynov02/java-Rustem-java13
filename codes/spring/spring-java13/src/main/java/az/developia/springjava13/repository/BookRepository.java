832ef23192f6f10711989a502deb01af95ed4ec9
RustamHuseynov02 <147059960+RustamHuseynov02@users.noreply.github.com>
2024-01-28 22:57:01 +0400

    Update BookRepository.java

--- codes/spring/spring-java13/src/main/java/az/developia/springjava13/repository/BookRepository.java
+++ codes/spring/spring-java13/src/main/java/az/developia/springjava13/repository/BookRepository.java
@@ -2,8 +2,8 @@ package az.developia.springjava13.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import az.developia.springjava13.component.Book;
+import az.developia.springjava13.entity.BookEntity;
 
-public interface BookRepository extends JpaRepository<Book, Integer> {
+public interface BookRepository extends JpaRepository<BookEntity, Integer> {
 
 }
